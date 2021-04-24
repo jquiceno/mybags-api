0b733745dd6ec101ae722d1becc6ca1bc0fb9ad5
Jquiceno <loncuster@gmail.com>
2021-04-24 17:07:53 +0200

    refactor routes file

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -5,77 +5,76 @@ const router = Router()
 
 const { Person } = require('../lib/Person')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const persons = await Person.getAll()
+router.route('/')
+  .get(async (req, res, next) => {
+    try {
+      const persons = await Person.getAll()
 
-    return res.json({
-      data: persons,
-      statusCode: 200
-    })
-  } catch (error) {
-    return next(error)
-  }
-})
+      return res.json({
+        data: persons,
+        statusCode: 200
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })
+  .post(async ({ body }, res, next) => {
+    try {
+      const persons = await Person.add(body)
 
-router.post('/', async ({ body }, res, next) => {
-  try {
-    const persons = await Person.add(body)
+      return res.json({
+        data: persons,
+        statusCode: 201
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })
 
-    return res.json({
-      data: persons,
-      statusCode: 201
-    })
-  } catch (error) {
-    return next(error)
-  }
-})
+router.route('/:personId')
+  .get(async ({ params }, res, next) => {
+    try {
+      const { personId } = params
+      const person = new Person(personId)
+      const personData = await person.get({
+        required: true
+      })
 
-router.get('/:personId', async ({ params }, res, next) => {
-  try {
-    const { personId } = params
-    const person = new Person(personId)
-    const personData = await person.get({
-      required: true
-    })
+      return res.json({
+        data: personData,
+        statusCode: 200
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })
+  .delete(async ({ params }, res, next) => {
+    try {
+      const { personId } = params
+      const person = new Person(personId)
+      const personData = await person.delete()
 
-    return res.json({
-      data: personData,
-      statusCode: 200
-    })
-  } catch (error) {
-    return next(error)
-  }
-})
+      return res.json({
+        data: personData,
+        statusCode: 200
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })
+  .put(async ({ params, body }, res, next) => {
+    try {
+      const { personId } = params
+      const person = new Person(personId)
+      const personData = await person.update(body)
 
-router.delete('/:personId', async ({ params }, res, next) => {
-  try {
-    const { personId } = params
-    const person = new Person(personId)
-    const personData = await person.delete()
-
-    return res.json({
-      data: personData,
-      statusCode: 200
-    })
-  } catch (error) {
-    return next(error)
-  }
-})
-
-router.put('/:personId', async ({ params, body }, res, next) => {
-  try {
-    const { personId } = params
-    const person = new Person(personId)
-    const personData = await person.update(body)
-
-    return res.json({
-      data: personData,
-      statusCode: 200
-    })
-  } catch (error) {
-    return next(error)
-  }
-})
+      return res.json({
+        data: personData,
+        statusCode: 200
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })
 
 module.exports = router
